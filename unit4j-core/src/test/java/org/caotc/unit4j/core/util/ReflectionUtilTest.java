81926e9d6fef5a8f2cc97233aeeaac15ecf0fe48
caotc <250622148@qq.com>
2019-07-10 22:42:12 +0800

    反射工具
    Signed-off-by: caotc <250622148@qq.com>

--- unit4j-core/src/test/java/org/caotc/unit4j/core/util/ReflectionUtilTest.java
+++ unit4j-core/src/test/java/org/caotc/unit4j/core/util/ReflectionUtilTest.java
@@ -1,6 +1,10 @@
 package org.caotc.unit4j.core.util;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
+import lombok.SneakyThrows;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -16,26 +20,54 @@ class ReflectionUtilTest {
   void propertySettersFromClass() {
     ReflectionUtil.propertySettersFromClass(Sub.class, false).forEach(System.out::println);
   }
+
+  @Test
+  @SneakyThrows
+  void jackson() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    Sub sub = new Sub();
+    String json = objectMapper.writeValueAsString(sub);
+    System.out.println(json);
+    sub = objectMapper.readValue(json, Sub.class);
+    System.out.println(sub);
+  }
 }
 
 
 @Data
+@FieldDefaults(makeFinal = false)
 abstract class Super {
 
-  protected String a = "super";
-
-//  public String getA() {
-//    return a;
-//  }
+  protected String stringField = "super";
 }
 
 @Data
+@FieldDefaults(makeFinal = false)
+@ToString(callSuper = true)
 class Sub extends Super {
 
-//  String a = "sub";
+  String stringField = "sub";
+  Number numberField = 88;
 
-  //  @Override
-  public String getA() {
-    return a;
+  public void setNumberField(int numberField) {
+    System.out.println("setNumberField int");
+    this.numberField = numberField;
+  }
+
+//  public void setNumberField(long numberField) {
+//    System.out.println("setNumberField long");
+//    this.numberField = numberField;
+//  }
+
+  public void setStringField(String stringField) {
+    this.stringField = stringField;
+  }
+
+  public String getStringField() {
+    return stringField;
+  }
+
+  public Number getNumberField() {
+    return numberField;
   }
 }
