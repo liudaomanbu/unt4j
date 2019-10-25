08ad2ce6b618b68a47f7f5b408eb64956d2d92b9
caotc <250622148@qq.com>
2019-10-25 23:28:03 +0800

    修改类名
    Signed-off-by: caotc <250622148@qq.com>

---
+++ unit4j-support/src/main/java/org/caotc/unit4j/support/annotation/WithUnit.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.caotc.unit4j.support.annotation;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.caotc.unit4j.core.constant.StringConstant;
+
+/**
+ * 单位注解,标注在属性和get方法上
+ *
+ * @author caotc
+ * @date 2019-07-17
+ * @since 1.0.0
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = {METHOD, FIELD})
+public @interface WithUnit {
+
+  /**
+   * 单位值,为id或者别名
+   */
+  String value() default StringConstant.EMPTY;
+
+  /**
+   * 单位属性
+   */
+  String property() default StringConstant.EMPTY;
+}
