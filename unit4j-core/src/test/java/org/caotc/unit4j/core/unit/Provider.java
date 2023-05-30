03f2a3d827c1ab945b761827ad7ce4bf876738ee
caotc <250622148@qq.com>
2023-05-30 19:57:56 +0800

    todo fix

--- unit4j-core/src/test/java/org/caotc/unit4j/core/unit/Provider.java
+++ unit4j-core/src/test/java/org/caotc/unit4j/core/unit/Provider.java
@@ -1,5 +1,6 @@
 package org.caotc.unit4j.core.unit;
 
+import com.google.common.collect.Maps;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.params.provider.Arguments;
@@ -19,17 +20,17 @@ public class Provider {
         Random random = new Random();
         int exponent = random.nextInt();
         return Stream.of(
-//                Arguments.of(UnitConstant.NON, UnitConstant.NON),
-//                Arguments.of(UnitConstant.METER, UnitConstant.METER),
-//                Arguments.of(UnitConstant.KILOGRAM, UnitConstant.KILOGRAM),
-//                Arguments.of(UnitConstant.JOULE, UnitConstant.JOULE),
-//                Arguments.of(UnitConstant.NEWTON.multiply(UnitConstant.METER), Unit.builder().componentToExponent(UnitConstant.KILOGRAM, 1).componentToExponent(UnitConstant.METER, 2).componentToExponent(UnitConstant.SECOND, -2).build()),
-//                Arguments.of(Unit.builder().componentToExponent(UnitConstant.METER, -1).build(), Unit.builder().componentToExponent(UnitConstant.METER, -1).build()),
-//                Arguments.of(Unit.builder().componentToExponent(UnitConstant.KILOGRAM, -1).build(), Unit.builder().prefix(Prefix.KILO.power(-1)).componentToExponent(UnitConstant.GRAM, -1).build()),
-//                Arguments.of(Unit.builder().componentToExponent(UnitConstant.JOULE, -1).build(), Unit.builder().componentToExponents(Maps.transformValues(UnitConstant.JOULE.componentToExponents(), value -> -value)).build()),
-//                Arguments.of(Unit.builder().componentToExponent(UnitConstant.METER, exponent).build(), Unit.builder().componentToExponent(UnitConstant.METER, exponent).build()),
-//                Arguments.of(Unit.builder().componentToExponent(UnitConstant.KILOGRAM, exponent).build(), Unit.builder().prefix(Prefix.KILO.power(exponent)).componentToExponent(UnitConstant.GRAM, exponent).build()),
-//                Arguments.of(Unit.builder().componentToExponent(UnitConstant.JOULE, exponent).build(), Unit.builder().componentToExponents(Maps.transformValues(UnitConstant.JOULE.componentToExponents(), value -> value * exponent)).build())
+                Arguments.of(UnitConstant.NON, UnitConstant.NON),
+                Arguments.of(UnitConstant.METER, UnitConstant.METER),
+                Arguments.of(UnitConstant.KILOGRAM, UnitConstant.KILOGRAM),
+                Arguments.of(UnitConstant.JOULE, UnitConstant.JOULE),
+                Arguments.of(UnitConstant.NEWTON.multiply(UnitConstant.METER), Unit.builder().componentToExponent(UnitConstant.KILOGRAM, 1).componentToExponent(UnitConstant.METER, 2).componentToExponent(UnitConstant.SECOND, -2).build()),
+                Arguments.of(Unit.builder().componentToExponent(UnitConstant.METER, -1).build(), Unit.builder().componentToExponent(UnitConstant.METER, -1).build()),
+                Arguments.of(Unit.builder().componentToExponent(UnitConstant.KILOGRAM, -1).build(), Unit.builder().prefix(Prefix.KILO.power(-1)).componentToExponent(UnitConstant.GRAM, -1).build()),
+                Arguments.of(Unit.builder().componentToExponent(UnitConstant.JOULE, -1).build(), Unit.builder().componentToExponents(Maps.transformValues(UnitConstant.JOULE.componentToExponents(), value -> -value)).build()),
+                Arguments.of(Unit.builder().componentToExponent(UnitConstant.METER, exponent).build(), Unit.builder().componentToExponent(UnitConstant.METER, exponent).build()),
+                Arguments.of(Unit.builder().componentToExponent(UnitConstant.KILOGRAM, exponent).build(), Unit.builder().prefix(Prefix.KILO.power(exponent)).componentToExponent(UnitConstant.GRAM, exponent).build()),
+                Arguments.of(Unit.builder().componentToExponent(UnitConstant.JOULE, exponent).build(), Unit.builder().componentToExponents(Maps.transformValues(UnitConstant.JOULE.componentToExponents(), value -> value * exponent)).build())
         );
     }
 
@@ -39,10 +40,10 @@ public class Provider {
         return Stream.concat(recursiveEqualedOriginalAndSimplifieds()
                         .flatMap(arguments -> Stream.of(Arguments.of(arguments.get()[0], true, arguments.get()[1]), Arguments.of(arguments.get()[0], false, arguments.get()[1]))),
                 Stream.of(
-//                        Arguments.of(Unit.builder().componentToExponent(UnitConstant.NEWTON.multiply(UnitConstant.METER), -1).build(), true, Unit.builder().componentToExponent(UnitConstant.KILOGRAM, -1).componentToExponent(UnitConstant.METER, -2).componentToExponent(UnitConstant.SECOND, 2).build()),
-                        Arguments.of(Unit.builder().componentToExponent(UnitConstant.NEWTON.multiply(UnitConstant.METER), -1).build(), false, Unit.builder().componentToExponent(UnitConstant.NEWTON, -1).componentToExponent(UnitConstant.METER, -1).build()),
-                        Arguments.of(Unit.builder().componentToExponent(UnitConstant.NEWTON.multiply(UnitConstant.METER), exponent).build(), true, Unit.builder().componentToExponent(UnitConstant.KILOGRAM, exponent).componentToExponent(UnitConstant.METER, 2 * exponent).componentToExponent(UnitConstant.SECOND, -2 * exponent).build()),
-                        Arguments.of(Unit.builder().componentToExponent(UnitConstant.NEWTON.multiply(UnitConstant.METER), exponent).build(), false, Unit.builder().componentToExponent(UnitConstant.NEWTON, exponent).componentToExponent(UnitConstant.METER, exponent).build())
+                        Arguments.of(Unit.builder().componentToExponent(Unit.builder().componentToExponent(UnitConstant.NEWTON, 1).componentToExponent(UnitConstant.METER, 1).build(), -1).build(), true, Unit.builder().componentToExponent(UnitConstant.KILOGRAM, -1).componentToExponent(UnitConstant.METER, -2).componentToExponent(UnitConstant.SECOND, 2).build()),
+                        Arguments.of(Unit.builder().componentToExponent(Unit.builder().componentToExponent(UnitConstant.NEWTON, 1).componentToExponent(UnitConstant.METER, 1).build(), -1).build(), false, Unit.builder().componentToExponent(UnitConstant.NEWTON, -1).componentToExponent(UnitConstant.METER, -1).build()),
+                        Arguments.of(Unit.builder().componentToExponent(Unit.builder().componentToExponent(UnitConstant.NEWTON, 1).componentToExponent(UnitConstant.METER, 1).build(), exponent).build(), true, Unit.builder().componentToExponent(UnitConstant.KILOGRAM, exponent).componentToExponent(UnitConstant.METER, 2 * exponent).componentToExponent(UnitConstant.SECOND, -2 * exponent).build()),
+                        Arguments.of(Unit.builder().componentToExponent(Unit.builder().componentToExponent(UnitConstant.NEWTON, 1).componentToExponent(UnitConstant.METER, 1).build(), exponent).build(), false, Unit.builder().componentToExponent(UnitConstant.NEWTON, exponent).componentToExponent(UnitConstant.METER, exponent).build())
                 ));
     }
 }
