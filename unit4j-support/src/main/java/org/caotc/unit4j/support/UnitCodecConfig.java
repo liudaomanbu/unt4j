8e871633f8ae0aaaca1c82bba37b916021b5cb17
caotc <cao19800207>
2023-08-22 00:16:38 +0800

    todo fix
    Signed-off-by: caotc <cao19800207>

--- unit4j-support/src/main/java/org/caotc/unit4j/support/UnitCodecConfig.java
+++ unit4j-support/src/main/java/org/caotc/unit4j/support/UnitCodecConfig.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.caotc.unit4j.support;
 
 import lombok.NonNull;
@@ -44,9 +60,8 @@ public class UnitCodecConfig {
    */
   @NonNull
   public String serialize(@NonNull Unit unit) {
-      //TODO 增加UnitSerializeStrategy
-//    return unit.alias(configuration(), type()).map(Alias::value)
-//            .orElseGet(() -> aliasUndefinedStrategy().execute(unit, configuration(), type()));
-      return "";
+    //TODO 增加UnitSerializeStrategy
+    return configuration().aliases(unit, type()).stream().map(Alias::value).findFirst()
+            .orElseGet(() -> aliasUndefinedStrategy().execute(unit, configuration(), type()));
   }
 }
